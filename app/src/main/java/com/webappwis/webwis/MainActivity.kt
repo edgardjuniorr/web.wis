c282f79a0ea336b3ac47a656674ef3d4baedbfc0
edgardjuniorr <edgard.juniormoraes@hotmail.com>
2023-10-22 20:50:46 -0300

    Novos botões na toolbar

--- app/src/main/java/com/webappwis/webwis/MainActivity.kt
+++ app/src/main/java/com/webappwis/webwis/MainActivity.kt
@@ -3,12 +3,14 @@ package com.webappwis.webwis
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.webappwis.webwis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +18,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val webView = binding.webView
+        webView = binding.webView
         webView.webViewClient = WebViewClient()
         webView.loadUrl("https://wisdomer.net.br")
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+
+        val backButton = binding.icBack
+        val forwardButton = binding.icFoword
+        val refreshButton = binding.icRefresh
+
+        backButton.setOnClickListener {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            }
+        }
+
+        forwardButton.setOnClickListener {
+            if (webView.canGoForward()) {
+                webView.goForward()
+            }
+        }
+
+        refreshButton.setOnClickListener {
+            webView.reload()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
